c8659560e4b9bcf2a687d6f4269b10de9f3314d4
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-20 15:39:42 -0300

    Animação das abas

--- src/components/SettingTabs/TabItem.tsx
+++ src/components/SettingTabs/TabItem.tsx
@@ -1,5 +1,7 @@
 'use client'
 import * as Tabs from '@radix-ui/react-tabs'
+import { motion } from 'framer-motion'
+
 export interface TabItemProps {
   value: string
   title: string
@@ -15,7 +17,10 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
       <span className="">{title}</span>
 
       {isSelected && (
-        <div className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700" />
+        <motion.div
+          layoutId="activeTab"
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
