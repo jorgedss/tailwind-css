ecb624e1bacc0e9d9ffabf9e18287320b2347663
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-20 09:45:24 -0300

    Logo e Input de busca

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/app/**/*.tsx'],
+  content: ['./src/***/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
