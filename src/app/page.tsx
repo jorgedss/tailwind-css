2ce4b9f668e93df8bef2a54ef88c6316244c08b0
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-21 09:27:28 -0300

    Upload do avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { SettingTabs } from '@/components/SettingTabs'
 
 import * as Input from '@/components/Input'
-import { Mail } from 'lucide-react'
+import { Mail, UploadCloud, User } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -78,12 +78,43 @@ export default function Home() {
           </div>
 
           <div className="grid gap-3  pt-5 grid-cols-form">
-            <label className="text-sm font-medium text-zinc-700">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700"
+            >
               Your photo
               <p className="text-sm font-normal text-zinc-500 mt-0.5">
                 This will be displayed on your profile.
               </p>
             </label>
+
+            <div className=" flex items-start gap-5">
+              <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+
+              <label
+                htmlFor="photo"
+                className=" group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
+              >
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                  <UploadCloud className="text-zinc-600 group-hover:text-violet-700" />
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="font-semibold text-violet-700">
+                      Click to upload
+                    </span>
+                    or drag and drop
+                  </span>
+                  <span className="text-sm">
+                    SVG, PNG, JPG or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+              <input type="file" className="sr-only" id="photo" />
+            </div>
           </div>
 
           <div className="grid gap-3  pt-5 grid-cols-form">
