0466ad628b9c3a4e648adf1a1fbd445c3fd63302
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-27 10:06:48 -0300

    Animando portfólio

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -3,12 +3,13 @@
 import { Trash2, UploadCloud } from 'lucide-react'
 import { UseFileInput } from './Root'
 import { formatBytes } from '@/utils/format-bytes'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function FileList() {
   const { files } = UseFileInput()
-
+  const [parent] = useAutoAnimate()
   return (
-    <div className=" mt-4 space-y-3">
+    <div ref={parent} className=" mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
