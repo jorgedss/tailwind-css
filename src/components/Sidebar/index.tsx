9c097b617986de7de8ad5f657e64ba8c8ee8d91a
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-28 11:04:51 -0300

    Responsividade da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -18,7 +18,7 @@ import * as Input from '../Input'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r  border-zinc-200 px-5 py-8 ">
+    <aside className="flex p-4 bottom-0 flex-col border-b gap-6 border-r  border-zinc-200  top-0  fixed left-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
       <Logo />
       <Input.Root>
         <Input.Prefix>
